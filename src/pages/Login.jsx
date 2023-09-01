ae91e8d297ae2b57e9d8e72a78482f787ddae23d
Mark <marcboluda@hotmail.com>
2023-09-01 11:29:09 +0200

    login done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,79 @@
-
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import service from "../services/service.config";
+import { AuthContext } from "../context/auth.context";
 
 function Login() {
+  const { verifyToken } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleUsernameChange = (e) => setUsername(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await service.post("/auth/login", {
+        username,
+        password,
+      });
+      console.log(response);
+
+      //   almacenamos el token en el LocalStorage
+      localStorage.setItem("authToken", response.data.authToken);
+
+      await verifyToken();
+
+      navigate("/swipe");
+      
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.status === 400) {
+        setErrorMessage(error.response.data.errorMessage);
+      } else {
+        navigate("/error");
+      }
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <h1>Log In</h1>
+
+      <form onSubmit={handleLogin}>
+        <label>Username:</label>
+        <input
+          type="username"
+          name="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+
+        <br />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+
+        <br />
+
+        <button type="submit">Login</button>
+
+        {errorMessage ? <p>{errorMessage}</p> : null}
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
