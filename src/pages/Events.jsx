3757b612c62a6949c989813eef7cb90be49acea9
Mark <marcboluda@hotmail.com>
2023-09-05 12:36:07 +0200

    event started

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,9 +1,36 @@
-
+import React, { useState, useEffect } from "react";
+import service from "../services/service.config";
 
 function Events() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await service.get("/events");
+        setEvents(response.data);
+      } catch (error) {
+        console.error("Error fetching events: ", error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
-    <div>Events</div>
-  )
+    <div>
+      <h2>Events</h2>
+      <ul>
+        {events.map((event) => (
+          <li key={event._id}>
+            <h3>{event.title}</h3>
+            <img src={event.image}  alt={event.name} />
+            <p>{event.location}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Events
+export default Events;
