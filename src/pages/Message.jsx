76946da683ed597267b151064556f11dfee48d63
OmarNavarro <omar.n.o.92@gmail.com>
2023-09-07 17:11:53 +0200

    message-fixed

--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import service from "../services/service.config";
 import { useParams } from "react-router-dom";
 
@@ -6,8 +6,8 @@ function Messages() {
   const { senderId, receiverId } = useParams();
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
-  console.log("senderId:", senderId);
-  console.log("receiverId:", receiverId);
+  const [visibleMessages, setVisibleMessages] = useState(12);
+  const messagesContainerRef = useRef(null);
 
   useEffect(() => {
     const fetchMessages = async () => {
@@ -15,9 +15,7 @@ function Messages() {
         const response = await service.get(
           `/user/messages/${senderId}/${receiverId}`
         );
-        console.log("Response from fetchMessages:", response.data);
         setMessages(response.data);
-        console.log("Messages after updating:", messages);
       } catch (error) {
         console.error("Error al obtener mensajes:", error);
       }
@@ -41,40 +39,58 @@ function Messages() {
     }
   };
 
+  const handleScroll = () => {
+  
+    if (
+      messagesContainerRef.current.scrollTop === 0 &&
+      visibleMessages < messages.length
+    ) {
+      setVisibleMessages(visibleMessages + 12);
+    }
+  };
+
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
       sendMessage();
     }
   };
-
   return (
     <div>
       <div className="logo">
-        <img src="/IronMeet logo-fotor-bg-remover-2023090792810.png" alt="Logo" />
+        <img
+          src="/IronMeet logo-fotor-bg-remover-2023090792810.png"
+          alt="Logo"
+        />
         <img src="/IRONMEET.PNG" alt="logo-slo" />
       </div>
-      <div className="message-container">
-  {messages.map((message) => (
-    <div
-      key={message._id}
-      className={`message ${
-        message.sender === senderId ? "user-message" : "message"
-      }`}
-    >
-      {message.text}
-    </div>
-  ))}
-      <div className="message-input">
-        <input
-          type="text"
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-          placeholder="Write your message here..."
-          onKeyDown={handleKeyDown}
-        />
-        <button onClick={sendMessage}>Send message</button>
+      <div
+        className="message-container-m"
+        onScroll={handleScroll}
+        ref={messagesContainerRef}
+      >
+        {messages
+          .slice(-visibleMessages)
+          .map((message) => (
+            <div
+              key={message._id}
+              className={`message ${
+                message.sender === senderId ? "user-message" : "message"
+              }`}
+            >
+              {message.text}
+            </div>
+          ))}
+        <div className="message-input">
+          <input
+            type="text"
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+            placeholder="Write your message here..."
+            onKeyDown={handleKeyDown}
+          />
+          <button onClick={sendMessage}>Send message</button>
+        </div>
       </div>
-</div>
     </div>
   );
 }
